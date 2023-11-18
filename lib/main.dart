224e2a6c4357c6f356797d5948d9f79c79280c4b
njiti <kamaukan77@gmail.com>
2023-11-18 14:15:59 +0300

    Full commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:minimal_ecommerce_app/pages/cart_page.dart';
+import 'package:minimal_ecommerce_app/pages/intro_page.dart';
+import 'package:minimal_ecommerce_app/pages/shop_page.dart';
+import 'package:minimal_ecommerce_app/themes/light_mode.dart';
+import 'package:provider/provider.dart';
+
+import 'models/shop.dart';
+
+void main() {
+  runApp(
+      ChangeNotifierProvider(
+          create: (context) => Shop(),
+          child: const MyApp(),
+      ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: IntroPage(),
+      theme: lightMode,
+      routes: {
+        '/intro_page': (context) => const IntroPage(),
+        '/shop_page': (context) => const ShopPage(),
+        '/cart_page': (context) => const CartPage(),
+      },
+    );
+
+  }
+}
---
+++ lib/models/shop.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:minimal_ecommerce_app/models/product.dart';
+
+class Shop extends ChangeNotifier {
+  // products for sale
+  final List<Product> _shop = [
+    // product 1
+    Product(
+      name: "Product 1",
+      price: 99.9,
+      description: "item description to describe the product more info here will look like this",
+      imagePath: 'assets/avocado.png'
+    ),
+    // product 2
+    Product(
+        name: "Product 2",
+        price: 99.9,
+        description: "item description..",
+        imagePath: 'assets/avocado.png'
+    ),
+    // product 3
+    Product(
+        name: "Product 3",
+        price: 99.9,
+        description: "item description..",
+        imagePath: 'assets/avocado.png'
+    ),
+    // product 4
+    Product(
+        name: "Product 4",
+        price: 99.9,
+        description: "item description..",
+        imagePath: 'assets/avocado.png',
+    ),
+  ];
+
+  // user cart
+  List<Product> _cart =[];
+
+  // get product list
+  List<Product> get shop => _shop;
+
+  // get user cart
+  List<Product> get cart => _cart;
+
+  // add item to cart
+  void addToCart(Product item) {
+    _cart.add(item);
+    notifyListeners();
+  }
+
+  // remove item from cart
+  void removeFromCart(Product item) {
+    _cart.remove(item);
+    notifyListeners();
+  }
+}
